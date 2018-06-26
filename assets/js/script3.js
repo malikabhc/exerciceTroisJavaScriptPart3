d0fc7a34539f15473511d393939572de1864b016
Malikabhc <malika.bouhcine@gmail.com>
2018-06-26 13:54:18 +0200

    correction - code plus court

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,12 +1,4 @@
-// Création de la fonction pour afficher le texte en vert
-function colorGreen() {
-  document.getElementById('text').style.color = 'green';
-}
-// Création de la fonction pour afficher le texte en rouge
-function colorRed() {
-  document.getElementById('text').style.color = 'red';
-}
-// Création de la fonction pour afficher le texte en bleu
-function colorBlue() {
-  document.getElementById('text').style.color = 'blue';
+// Création de la fonction pour afficher le texte en couleur
+function changeColor(color) {
+  document.getElementById('text').style.color = color;
 }
